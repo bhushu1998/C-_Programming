2074bff99953edc68896a90da0e52c6c60e38337
bhushu1998 <50379824+bhushu1998@users.noreply.github.com>
2019-06-06 13:03:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("my first console program");
+            Console.ReadLine();
+        }
+    }
+}
